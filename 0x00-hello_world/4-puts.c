4c03bd84d6f46db73a90b7b2d017117c9095cb06
Oyedolacode <segunzyshegszy12@gmail.com>
2022-03-11 14:12:06 +0100

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-/**
- *main - function
- *Return: valve 0
- */
+/** * main - Entry point * * Return: Always 0 (Success) */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+{puts("\"Programming is like building a multilingual puzzle");	return (0);}
